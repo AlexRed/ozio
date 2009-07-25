4c79366c4bafe07a1da22bcf5897076431b0e096
vanbertol <vanbertol@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2009-07-25 18:51:10 +0000

    Rimossi file installazione dalla root della componente

--- trunk/uninstall.oziogallery2.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined( '_JEXEC' ) or die( 'Restricted access' );
-function com_uninstall()
-{
-?>  
-<div class="header">The component Ozio Gallery has been uninstalled successfully.</div>
-<?php
-}
-?>
