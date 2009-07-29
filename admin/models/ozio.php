7925082132ed7c1d08dae5bd70d92df4f1f39e64
vanbertol <vanbertol@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2009-07-29 12:06:50 +0000

    Correzione Problema voci menu cestinate , integrazione edit al volo gruppo menu associato alla voce di menu

--- admin/models/ozio.php
+++ admin/models/ozio.php
@@ -15,19 +15,20 @@ class OzioModelOzio extends JModel
 	{
 	
 	
-		$query = 'SELECT id, componentid, name, link'
-				. ' FROM #__menu'
-				. ' WHERE (link = "index.php?option=com_oziogallery2&view=01tiltviewer" 
-						OR link = "index.php?option=com_oziogallery2&view=02flashgallery"
-						OR link = "index.php?option=com_oziogallery2&view=03imagin"
-						OR link = "index.php?option=com_oziogallery2&view=04carousel"
-						OR link = "index.php?option=com_oziogallery2&view=05imagerotator"
-						OR link = "index.php?option=com_oziogallery2&view=06accordion"	
-						OR link = "index.php?option=com_oziogallery2&view=07flickrslidershow"
-						OR link = "index.php?option=com_oziogallery2&view=08flickrphoto"							
+		$query = 'SELECT i.id, i.componentid, i.name, i.link, i.menutype, men.title AS nomemenu, men.id AS menuid'
+				. ' FROM #__menu AS i'
+				. ' LEFT JOIN #__menu_types AS men ON men.menutype = i.menutype'				
+				. ' WHERE (i.link = "index.php?option=com_oziogallery2&view=01tiltviewer" 
+						OR i.link = "index.php?option=com_oziogallery2&view=02flashgallery"
+						OR i.link = "index.php?option=com_oziogallery2&view=03imagin"
+						OR i.link = "index.php?option=com_oziogallery2&view=04carousel"
+						OR i.link = "index.php?option=com_oziogallery2&view=05imagerotator"
+						OR i.link = "index.php?option=com_oziogallery2&view=06accordion"	
+						OR i.link = "index.php?option=com_oziogallery2&view=07flickrslidershow"
+						OR i.link = "index.php?option=com_oziogallery2&view=08flickrphoto"							
 				)'
-				. ' AND published = 1'				
-				. ' ORDER BY name DESC'
+				. ' AND i.published = 1'				
+				. ' ORDER BY i.name DESC'
 				;
 
 		$this->_db->SetQuery($query);
@@ -51,7 +52,8 @@ class OzioModelOzio extends JModel
 						OR link = "index.php?option=com_oziogallery2&view=07flickrslidershow"
 						OR link = "index.php?option=com_oziogallery2&view=08flickrphoto"							
 				)'
-				. ' AND published != 1'				
+				. ' AND published  = 0'	
+				. ' AND published != -2'				
 				. ' ORDER BY name DESC'
 				;
 
