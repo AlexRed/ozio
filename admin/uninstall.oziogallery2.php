a0f82c7f9473ba320a97d49e9224193cced0977d
vanbertol <vanbertol@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2009-08-10 19:02:42 +0000

    Fix disintallazione per eliminazione plugin

--- admin/uninstall.oziogallery2.php
+++ admin/uninstall.oziogallery2.php
@@ -1,9 +1,46 @@
 <?php
 defined( '_JEXEC' ) or die( 'Restricted access' );
-function com_uninstall()
-{
+/*
+function deleteDir($dir){
+    if (substr($dir, strlen($dir)-1, 1)!= '/')
+        $dir .= '/';
+    if ($handle = opendir($dir))
+    {
+        while ($obj = readdir($handle))
+        {
+            if ($obj!= '.' && $obj!= '..')
+            {
+                if (is_dir($dir.$obj))
+                {
+                    if (!deleteDir($dir.$obj))
+                        return false;
+                }
+                elseif (is_file($dir.$obj))
+                {
+                    if (!unlink($dir.$obj))
+                        return false;
+                }
+            }
+        }
+        closedir($handle);
+        if (!@rmdir($dir))
+            return false;
+        return true;
+    }
+    return false;
+}
+*/
+function com_uninstall(){
+  
+	$db=JFactory::getDBO();
+    unlink(JPATH_ROOT . DS . 'plugins' . DS . 'content' . DS . 'ozio.php');
+    unlink(JPATH_ROOT . DS . 'plugins' . DS . 'content' . DS . 'ozio.xml');
+    $db->setQuery("DELETE FROM #__plugins WHERE name='Content - OzioGallery2'");
+    $db->query();
+	
+
 ?>  
-<div class="header">The component Ozio Gallery has been uninstalled successfully.</div>
+<div class="header">The component Ozio Gallery and Plugin Content - OzioGallery2 has been uninstalled successfully.</div>
 <?php
 }
 ?>
