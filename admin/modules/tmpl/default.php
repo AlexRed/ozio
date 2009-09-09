23eeaef0d88a199e484efc23bfe137a8bb6be64c
vanbertol <vanbertol@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2009-09-09 15:50:46 +0000

    Ripensamento: Rimossa installazione automatica del modulo

--- admin/modules/tmpl/default.php
+++
@@ -1,2 +0,0 @@
-<?php defined('_JEXEC') or die('Restricted access'); // no direct access ?>
-<?php echo $output; ?>
--- admin/uninstall.oziogallery2.php
+++ admin/uninstall.oziogallery2.php
@@ -38,15 +38,6 @@ function com_uninstall(){
     $db->setQuery("DELETE FROM #__plugins WHERE name='Content - OzioGallery2'");
     $db->query();
 
-    unlink(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2' . DS . 'tmpl' . DS . 'default.php');
-    unlink(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2' . DS . 'tmpl' . DS . 'index.html');
-    rmdir(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2' . DS . 'tmpl');
-    unlink(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2' . DS . 'index.html');	
-    unlink(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2' . DS . 'helper.php');
-    unlink(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2' . DS . 'mod_ozio2.php');
-    unlink(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2' . DS . 'mod_ozio2.xml');	
-    rmdir(JPATH_ROOT . DS . 'modules' . DS . 'mod_ozio2');
-	
 
 ?>  
 <div class="header">The component Ozio Gallery and Plugin Content - OzioGallery2 has been uninstalled successfully.</div>
