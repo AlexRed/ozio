77dde360b9c53f26ac381e4cc1b9c67abdec086f
alexred.ozio <alexred.ozio@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2010-04-23 14:44:09 +0000

    inserito il wmode: "opaque" per la skin cooliris

--- site/views/10cooliris/tmpl/default.php
+++ site/views/10cooliris/tmpl/default.php
@@ -38,7 +38,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 <table width="100%" align="<?php echo $this->table ?>">
 	<tr>
 		<td>
-			<div id="oziowall" class="oziofloat">  aaaaa</div>
+			<div id="oziowall" class="oziofloat">  <a href="http://www.macromedia.com/go/getflashplayer">Get the Flash Player</a> to see this gallery.</div>
     <script>
         var flashvars = {
 		<?php if  	  ( $this->flickr == 1 ) : ?>	
@@ -52,6 +52,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 		<?php endif; ?>
         var params = {
              allowFullScreen: "true",
+			 wmode: "opaque",
              allowscriptaccess: "always"
         };
         swfobject.embedSWF("<?php echo JURI::root() ?>components/com_oziogallery2/skin/cooliris/cooliris.swf",
