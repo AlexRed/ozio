3f6d7a2087e1fc36bca12d784e3f991342fe3c07
vanbertol <vanbertol@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2010-06-01 23:38:12 +0000

    Parametro info solo in tilt3d xml per test e modifica versione da 2 a 3 nei restanti file

--- trunk/3/admin/models/fields/coolirisreset.php
+++ trunk/3/admin/models/fields/coolirisreset.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* This file is part of Ozio Gallery 2.
+* This file is part of Ozio Gallery 3
 *
-* Ozio Gallery 2 is free software: you can redistribute it and/or modify
+* Ozio Gallery 3 is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 2 of the License, or
 * (at your option) any later version.
