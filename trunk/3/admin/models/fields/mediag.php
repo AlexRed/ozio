51a5f314da9edd9af2b2480f8b4828371678b18a
vanbertol <vanbertol@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2010-06-02 11:45:13 +0000

    modificate tutte le skin. Ci sono tre descrizioni nei file lingua da creare

--- trunk/3/admin/models/fields/mediag.php
+++ trunk/3/admin/models/fields/mediag.php
@@ -21,15 +21,15 @@
 
 defined('JPATH_BASE') or die;
 
-class JFormFieldTilt3d extends JFormField
+class JFormFieldMediag extends JFormField
 {
-	protected $type = 'Tilt3d';
+	protected $type = 'Mediag';
 
 	protected function getInput()
 	{
 
 		return '<div style="padding:5px 5px 5px 0; "><p style="font-size:98%; color:#666; padding: 10px 5px 0px 2px;"><br />' .
-				JText::_('COM_OZIOGALLERY3_OZIO_TILTVIEWER_SKIN_DESC').
+				JText::_('COM_OZIOGALLERY3_OZIO_MEDIAGALLERY_SKIN_DESC').
 				'</p></div>';
 	}
 }
