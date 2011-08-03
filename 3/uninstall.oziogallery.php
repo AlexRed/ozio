703929b1f986ff8a12de66e76b9ff9d543319f2e
alexred%alexred.com@gtempaccount.com <alexred%alexred.com@gtempaccount.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2011-08-03 14:51:22 +0000

    corretto il problema per far disinstallare anche il plugin quando si disinstalla il componente. Grazie a corephp.com

---
+++ 3/uninstall.oziogallery.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* This file is part of Ozio Gallery 3.
+*
+* Ozio Gallery 3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* Foobar is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @copyright Copyright (C) 2010 Open Source Solutions S.L.U. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see RT-LICENSE.php
+*/
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+$db = JFactory::getDBO ();
+$db->setQuery ( "SELECT extension_id FROM `#__extensions` WHERE name LIKE '%ozio%' AND type='plugin' " );
+$res = $db->loadAssocList();
+
+foreach ( $res as $plugin ){
+	$installer = clone JInstaller::getInstance ();
+	$installer->uninstall('plugin', $plugin['extension_id']);
+}
