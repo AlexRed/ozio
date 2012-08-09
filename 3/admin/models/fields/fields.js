b5c3ba1c8a2a11887a34a1a8533f3444e7bed40e
demis.palma@gmail.com <demis.palma@gmail.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2012-08-09 18:25:57 +0000

    Fixed plugin upgrade script

--- 3/admin/models/fields/fields.js
+++ 3/admin/models/fields/fields.js
@@ -14,9 +14,26 @@ function TrigAllSelextChange()
 	{
 		var select = selects[i];
 		if (select.getAttribute('class') == 'selext')
-		{
+			{
 			select.onchange(select);
 		}
+	}
+}
+
+
+function OnUseridChanged()
+{
+	var input = $('jform_params_userid');
+	var re = new RegExp('^[0-9]{21}');
+
+	if (input.value.match(re))
+	{
+		LoadAlbums();
+	}
+	else
+	{
+		alert("COM_OZIOGALLERY3_ERR_INVALID_USER");
+		input.value = input.defaultValue;
 	}
 }
 
@@ -27,19 +44,24 @@ function LoadAlbums()
 	// Caricamento album dopo la selezione dell'utente
 	// AlexRed segnala che il caricamento sembra fallire. Problema forse legato all'installazione in una sottocartella?
 	// ...
-	return;
+	//return;
 
 	var input = $('jform_params_userid');
-	var board = $('jform_params_gallery_id-lbl');
 
 	var xhrOnRequest = function()
 	{
-		board.set('text', 'Loading...');
+		// Hide the select
+		$('jform_params_gallery_id').hide();
+		// Show the loader
+		$('jform_params_gallery_id_loader').show();
 	};
 
 	var xhrOnComplete = function(response)
 	{
-		board.empty();
+		// Show the select
+		$('jform_params_gallery_id').show();
+		// Hide the loader
+		$('jform_params_gallery_id_loader').hide();
 	};
 
 	var xhrOnSuccess = function(responseText, responseXML)
@@ -47,23 +69,55 @@ function LoadAlbums()
 		var select = $('jform_params_gallery_id');
 		// Clear previous options
 		select.options.length = 0;
+		select.setStyle('width', '50%');
 
 		// Abilitazione combo
 		// select.style.display = 'none';
 
-		var albums = responseXML.getElements('entry');
+		var numbuzz = 0;
+		var feed = responseXML.childNodes[0];
+		var albums = feed.getElements('entry');
 		for (var a = 0; a < albums.length; ++a)
 		{
-			var title = albums[a].getElement('title').get('text');
-			var id = albums[a].getElement('gphoto\\:id').get('text');
-			var content = albums[a].getElement('gphoto\\:numphotos').get('text');
-			addoption(select, id, title + ' (' + content + ')');
+			var album = albums[a];
+			var title = album.getElement('title').textContent;
+			var id, numphotos;
+
+			var albumtype = 'COM_OZIOGALLERY3_ALBUMTYPE_NONE';
+			// getElement() doesn't work on namespaces
+			//title = albums[a].getElement('title').get('text');
+			//id = albums[a].getElement('gphoto\\:id').get('text');
+			//numphotos = albums[a].getElement('gphoto\\:numphotos').get('text');
+			for (var c = 0; c < album.childNodes.length; ++c)
+			{
+				var tagName = album.childNodes[c].tagName;
+				if (album.childNodes[c].tagName == 'gphoto:id') id = album.childNodes[c].textContent;
+				if (album.childNodes[c].tagName == 'gphoto:numphotos') numphotos = album.childNodes[c].textContent;
+				//if (album.childNodes[c].tagName == 'gphoto:albumType') albumtype = 'COM_OZIOGALLERY3_ALBUMTYPE_' + album.childNodes[c].textContent.toUpperCase();
+				if (album.childNodes[c].tagName == 'gphoto:albumType')
+					{
+					title = 'COM_OZIOGALLERY3_ALBUMTYPE_' + album.childNodes[c].textContent.toUpperCase();
+				}
+			}
+
+			if (title == 'COM_OZIOGALLERY3_ALBUMTYPE_BUZZ')
+				{
+				numbuzz += parseInt(numphotos);
+			}
+			else
+				{
+				addoption(select, id, title + ' (' + numphotos + ')');
+			}
 		}
+
+		addoption(select, "Buzz", 'COM_OZIOGALLERY3_ALBUMTYPE_BUZZ' + ' (' + numbuzz + ')');
+
 		// Todo: Predisposto ma lasciato incompleto
 		// Una soluzione migliore della voce "Altro..." con attivazione della casella di immisisone manuale, e' una SelectBox con funzioni di InputBox
 		// come quella utilizzata dal selettore posizione modulo in Joomla 1.5
 		// ...
-		addoption(select, 0, 'Selezione manuale');
+		//addoption(select, 0, 'Selezione manuale');
+		SelectCurrentAlbum();
 	};
 
 	var xhrOnFailure = function(response)
@@ -85,7 +139,6 @@ function LoadAlbums()
 
 	var request = new Request(options);
 	request.send();
-
 }
 
 function addoption(select, value, text)
@@ -93,7 +146,6 @@ function addoption(select, value, text)
 	var option = document.createElement('option');
 	option.value = value;
 	option.text = text;
-
 	try
 	{
 		// standards compliant; doesn't work in IE
@@ -109,24 +161,19 @@ function addoption(select, value, text)
 
 function SelectCurrentAlbum()
 {
-	var input = $('jform_params_userid');
 	var select = $('jform_params_gallery_id');
+	var spia = $('jform_params_gallery_id_selected');
 	var options = select.options;
 
 	for (var i = 0; i < options.length; ++i)
 	{
-		// Todo: Predisposto ma lasciato incompleto
-		// Confrontare con il valore memorizzato nel campo
-		// per ottenere questo valore bisogna riportarlo (nascosto) nella pagina con un id rintracciabile
-		// della trascrizione se ne deve occupare JFormFieldListGalleries aggiungendo una label non visibile
-		if (/*input.value*/ '5745621478740744081' == options[i].value)
-			{
+		if (options[i].value == spia.innerHTML)
+		{
 			options[i].selected = true;
 		}
 	}
 
 }
 
-//window.addEvent('domready', LoadAlbums);
-//window.addEvent('domready', SelectCurrentAlbum);
+window.addEvent('domready', LoadAlbums);
 //window.addEvent('domready', TrigAllSelextChange);
--- 3/file.script.php
+++ 3/file.script.php
@@ -21,95 +21,108 @@
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-class Com_OzioGallery3InstallerScript 
+class Com_OzioGallery3InstallerScript
 {
 
-	function install($parent) 
+	function install($parent)
 	{
-			
-			//Inzio attivazione Plugin
-			$manifest 	= $parent->get("manifest");
-			$parent 	= $parent->getParent();
-			$source 	= $parent->getPath("source");
-			$installer 	= new JInstaller();
-			 
-			foreach($manifest->plugins->plugin as $plugin) 
-			{
-				$attributes = $plugin->attributes();
-				$plg = $source . DS . $attributes['folder'].DS.$attributes['plugin'];
-				$installer->install($plg);
-			}
-			 $db = JFactory::getDbo();
-			$tableExtensions = $db->nameQuote("#__extensions");
-			$columnEnabled   = $db->nameQuote("enabled");			
-			$columnElement   = $db->nameQuote("element");
-			$columnType      = $db->nameQuote("type");
 
-			 // Attiva plugin
-			$db->setQuery(
-				"UPDATE  $tableExtensions  SET $columnEnabled=1  WHERE $columnElement='ozio'   AND  $columnType='plugin'"
-			);
-			$db->query(); 
-	
-			//Inzio creazione cartelle
-			$folder[0][0]	=	'images' . DS . 'oziogallery3' . DS ;
-			$folder[0][1]	= 	JPATH_ROOT . DS .  $folder[0][0];
-			$folder[0][0]	=	'images' . DS . 'oziodownload' . DS ;
-			$folder[0][2]	= 	JPATH_ROOT . DS .  $folder[0][0];
-			$file 		= "index.html";
-			$file2 		= "info.png";	
-			$source 	= 	JPATH_ROOT . DS . 'components' . DS . 'com_oziogallery3';
-			$source2 	= 	JPATH_ROOT . DS . 'components' . DS . 'com_oziogallery3' . DS . 'assets';	
-			$dest 		=   JPATH_ROOT . DS . 'images' . DS . 'oziogallery3';	
-			$dest2 		=   JPATH_ROOT . DS . 'images' . DS . 'oziodownload';
-			
-			$message = '';
-			$error	 = array();
-			foreach ($folder as $key => $value)
-			{
-				if (!JFolder::exists( $value[1]))
-				{
-					if (JFolder::create( $value[1], 0755 ) && JFolder::create( $value[2], 0755 ) && @copy($source. DS .$file,$dest. DS .$file) && @copy($source. DS .$file,$dest2. DS .$file) && @copy($source2. DS .$file2,$dest. DS .$file2))
-					{
+		//Inzio attivazione Plugin
+		$manifest 	= $parent->get("manifest");
+		$parent 	= $parent->getParent();
+		$source 	= $parent->getPath("source");
+		$installer 	= new JInstaller();
 
-						$message .= '<p><b><span style="color:#009933">Folder</span> ' . $value[0] 
-								   .' <span style="color:#009933">created!</span></b></p>';
-						$error[] = 0;
-					}	 
-					else
-					{
-						$message .= '<p><b><span style="color:#CC0033">Folder</span> ' . $value[0]
-								   .' <span style="color:#CC0033">creation failed!</span></b> Please create it manually.</p>';
-						$error[] = 1;
-					}
-				}
-				else//Folder exist
+		foreach($manifest->plugins->plugin as $plugin)
+		{
+			$attributes = $plugin->attributes();
+			$plg = $source . DS . $attributes['folder'].DS.$attributes['plugin'];
+			$installer->install($plg);
+		}
+		$db = JFactory::getDbo();
+		$tableExtensions = $db->nameQuote("#__extensions");
+		$columnEnabled   = $db->nameQuote("enabled");
+		$columnElement   = $db->nameQuote("element");
+		$columnType      = $db->nameQuote("type");
+
+		// Attiva plugin
+		$db->setQuery(
+		"UPDATE  $tableExtensions  SET $columnEnabled=1  WHERE $columnElement='ozio'   AND  $columnType='plugin'"
+		);
+		$db->query();
+
+		//Inzio creazione cartelle
+		$folder[0][0]	=	'images' . DS . 'oziogallery3' . DS ;
+		$folder[0][1]	= 	JPATH_ROOT . DS .  $folder[0][0];
+		$folder[0][0]	=	'images' . DS . 'oziodownload' . DS ;
+		$folder[0][2]	= 	JPATH_ROOT . DS .  $folder[0][0];
+		$file 		= "index.html";
+		$file2 		= "info.png";
+		$source 	= 	JPATH_ROOT . DS . 'components' . DS . 'com_oziogallery3';
+		$source2 	= 	JPATH_ROOT . DS . 'components' . DS . 'com_oziogallery3' . DS . 'assets';
+		$dest 		=   JPATH_ROOT . DS . 'images' . DS . 'oziogallery3';
+		$dest2 		=   JPATH_ROOT . DS . 'images' . DS . 'oziodownload';
+
+		$message = '';
+		$error	 = array();
+		foreach ($folder as $key => $value)
+		{
+			if (!JFolder::exists( $value[1]))
+			{
+				if (JFolder::create( $value[1], 0755 ) && JFolder::create( $value[2], 0755 ) && @copy($source. DS .$file,$dest. DS .$file) && @copy($source. DS .$file,$dest2. DS .$file) && @copy($source2. DS .$file2,$dest. DS .$file2))
 				{
-					$message .= '<p><b><span style="color:#009933">Folder</span> ' . $value[0] 
-								   .' <span style="color:#009933">exists!</span></b></p>';
+
+					$message .= '<p><b><span style="color:#009933">Folder</span> ' . $value[0]
+					.' <span style="color:#009933">created!</span></b></p>';
 					$error[] = 0;
 				}
-
+				else
+				{
+					$message .= '<p><b><span style="color:#CC0033">Folder</span> ' . $value[0]
+					.' <span style="color:#CC0033">creation failed!</span></b> Please create it manually.</p>';
+					$error[] = 1;
+				}
 			}
-	
+			else//Folder exist
+			{
+				$message .= '<p><b><span style="color:#009933">Folder</span> ' . $value[0]
+				.' <span style="color:#009933">exists!</span></b></p>';
+				$error[] = 0;
+			}
+
+		}
+
 		echo '<p>Congratulations! Ozio Gallery 3 Component for Joomla 1.6/1.7/2.5! has been installed successfully</p>';
 	}
 
-	function uninstall($parent) 
+	function uninstall($parent)
 	{
-	
+
 		echo '<p>The component Ozio Gallery 3 Component for Joomla 1.6/1.7/2.5! has been uninstalled successfully.</p>';
 	}
 
-	function update($parent) 
+
+	function update($parent)
+	{
+		$manifest 	= $parent->get("manifest");
+		$parent 	= $parent->getParent();
+		$source 	= $parent->getPath("source");
+		$installer 	= new JInstaller();
+
+		foreach($manifest->plugins->plugin as $plugin)
+		{
+			$attributes = $plugin->attributes();
+			$plg = $source . "/" . $attributes['folder'] . "/" . $attributes['plugin'];
+			$installer->install($plg);
+		}
+	}
+
+
+	function preflight($type, $parent)
 	{
 	}
 
-	function preflight($type, $parent) 
-	{
-	}
-
-	function postflight($type, $parent) 
+	function postflight($type, $parent)
 	{
 	}
 }
