aba4c99cf3945c14f2f604f283da330f3244c480
demis.palma@gmail.com <demis.palma@gmail.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2012-09-12 15:52:03 +0000

    Editor button for plugin code part 2.

---
+++ trunk/3/admin/views/galleries/view.html.php
@@ -0,0 +1,44 @@
+<?php defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+class OzioViewGalleries extends JView
+{
+	protected $items;
+	protected $pagination;
+	protected $state;
+
+	public function display($tpl = null)
+	{
+		$this->items		= $this->get('Items');
+		$this->pagination	= $this->get('Pagination');
+		$this->state		= $this->get('State');
+		$this->authors		= $this->get('Authors');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			JError::raiseError(500, implode("\n", $errors));
+			return false;
+		}
+
+		// Levels filter.
+		$options	= array();
+		$options[]	= JHtml::_('select.option', '1', JText::_('J1'));
+		$options[]	= JHtml::_('select.option', '2', JText::_('J2'));
+		$options[]	= JHtml::_('select.option', '3', JText::_('J3'));
+		$options[]	= JHtml::_('select.option', '4', JText::_('J4'));
+		$options[]	= JHtml::_('select.option', '5', JText::_('J5'));
+		$options[]	= JHtml::_('select.option', '6', JText::_('J6'));
+		$options[]	= JHtml::_('select.option', '7', JText::_('J7'));
+		$options[]	= JHtml::_('select.option', '8', JText::_('J8'));
+		$options[]	= JHtml::_('select.option', '9', JText::_('J9'));
+		$options[]	= JHtml::_('select.option', '10', JText::_('J10'));
+
+		$this->f_levels = $options;
+
+		if ($this->getLayout() !== 'modal') return;
+
+		parent::display($tpl);
+	}
+
+}
