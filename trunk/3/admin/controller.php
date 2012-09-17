97a3bd5722c58c8f5c4c1c7dda5919d8d28ec4df
demis.palma@gmail.com <demis.palma@gmail.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2012-09-17 19:23:43 +0000

    Bug Fixes during upgrade

--- trunk/3/admin/controller.php
+++ trunk/3/admin/controller.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* This file is part of Ozio Gallery 3
+* This file is part of Ozio Gallery
 *
 * Ozio Gallery 3 is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
