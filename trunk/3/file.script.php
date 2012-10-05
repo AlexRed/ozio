5750db803b15e28eac461b67aea1d94a8b8e05de
demis.palma@gmail.com <demis.palma@gmail.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2012-10-05 10:36:08 +0000

    Removed creation of images directory

--- trunk/3/file.script.php
+++ trunk/3/file.script.php
@@ -54,46 +54,6 @@ class Com_OzioGallery3InstallerScript
 		);
 		$db->query();
 
-		//Inzio creazione cartelle
-		$folder[0][0]	=	'images' . DS . 'oziogallery3' . DS ;
-		$folder[0][1]	= 	JPATH_ROOT . DS .  $folder[0][0];
-		$folder[0][0]	=	'images' . DS . 'oziodownload' . DS ;
-		$folder[0][2]	= 	JPATH_ROOT . DS .  $folder[0][0];
-		$file 		= "index.html";
-		$file2 		= "info.png";
-		$source 	= 	JPATH_ROOT . DS . 'components' . DS . 'com_oziogallery3';
-		$source2 	= 	JPATH_ROOT . DS . 'components' . DS . 'com_oziogallery3' . DS . 'assets';
-		$dest 		=   JPATH_ROOT . DS . 'images' . DS . 'oziogallery3';
-		$dest2 		=   JPATH_ROOT . DS . 'images' . DS . 'oziodownload';
-
-		$message = '';
-		$error	 = array();
-		foreach ($folder as $key => $value)
-		{
-			if (!JFolder::exists( $value[1]))
-			{
-				if (JFolder::create( $value[1], 0755 ) && JFolder::create( $value[2], 0755 ) && @copy($source. DS .$file,$dest. DS .$file) && @copy($source. DS .$file,$dest2. DS .$file) && @copy($source2. DS .$file2,$dest. DS .$file2))
-				{
-
-					$message .= '<p><b><span style="color:#009933">Folder</span> ' . $value[0]
-					.' <span style="color:#009933">created!</span></b></p>';
-					$error[] = 0;
-				}
-				else
-				{
-					$message .= '<p><b><span style="color:#CC0033">Folder</span> ' . $value[0]
-					.' <span style="color:#CC0033">creation failed!</span></b> Please create it manually.</p>';
-					$error[] = 1;
-				}
-			}
-			else//Folder exist
-			{
-				$message .= '<p><b><span style="color:#009933">Folder</span> ' . $value[0]
-				.' <span style="color:#009933">exists!</span></b></p>';
-				$error[] = 0;
-			}
-		}
-
 		$this->message();
 	}
 
