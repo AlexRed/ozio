4ad3083f784de4de554bbed578c3c3bfcf6822ad
www.joomla.it@gmail.com <www.joomla.it@gmail.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2013-06-14 13:03:30 +0000

    Inserito un alt ad alcune icone per la corretta validazione W3C

--- 4/site/views/00fuerte/tmpl/default.php
+++ 4/site/views/00fuerte/tmpl/default.php
@@ -53,9 +53,9 @@
 	<div id="oziobottombar" class="load-item oziobar">
 		<div id="controls">
 
-			<a id="play-button" class="oziobutton" <?php echo $this->play_button_style; ?>><img id="pauseplay" src="<?php echo JUri::base(true); ?>/components/com_oziogallery3/views/00fuerte/img/pause.png"/></a>
+			<a id="play-button" class="oziobutton" <?php echo $this->play_button_style; ?>><img id="pauseplay" src="<?php echo JUri::base(true); ?>/components/com_oziogallery3/views/00fuerte/img/pause.png" alt="stop" /></a>
 			<?php if ($this->Params->get("fullsize_button", true)) { ?>
-				<a id="view-button" class="oziobutton"><img src="<?php echo JUri::base(true); ?>/components/com_oziogallery3/views/00fuerte/img/view.png"/></a>
+				<a id="view-button" class="oziobutton"><img src="<?php echo JUri::base(true); ?>/components/com_oziogallery3/views/00fuerte/img/view.png" alt="view" /></a>
 				<?php } ?>
 
 			<!--Slide counter-->
