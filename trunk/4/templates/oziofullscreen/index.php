2d1b2e00fcae45e6a998006391ffcd74a9aa6722
www.joomla.it@gmail.com <www.joomla.it@gmail.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2013-09-25 12:34:02 +0000

    Ecco i file del template per il fullscreen

---
+++ trunk/4/templates/oziofullscreen/index.php
@@ -0,0 +1,18 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE html>
+<html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<head>
+<script type='text/javascript'>
+var ozio_fullscreen=1;
+</script>
+<jdoc:include type="head" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/oziofullscreen/css/template.css" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/oziofullscreen/css/oziofullscreen.css" type="text/css" />
+</head>
+<body>
+<a class="close_fullscreen" href="<?php echo $this->baseurl ?>">
+<img src="<?php echo $this->baseurl; ?>/templates/oziofullscreen/images/close.gif">
+</a>
+<jdoc:include type="component" />
+</body>
+</html>
