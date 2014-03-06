42bfdc36e838b564168e187dfa9f185b6144b78b
www.joomla.it@gmail.com <www.joomla.it@gmail.com@38635798-6f8e-11de-b374-81e7cb8b6ca2>
2014-03-06 11:11:13 +0000

    i file della nuova Skin

---
+++ trunk/4/site/views/nano/js/nano-starter.js
@@ -0,0 +1,67 @@
+<?php
+$non_printable_separator="\x16";
+?>
+jQuery( document ).ready(function( $ ) {
+	$("#nanoGallery").nanoGallery({
+		locationHash: <?php echo json_encode(intval($this->Params->get("ozio_nano_locationHash", "1"))); ?>,
+		viewerDisplayLogo: false,
+		//thumbnailHeight può essere anche auto
+		thumbnailHeight: <?php echo json_encode( $this->Params->get("ozio_nano_thumbnailHeight_kind","fixed")=="fixed"?intval($this->Params->get("ozio_nano_thumbnailHeight", "154")):"auto" ); ?>,
+		thumbnailWidth: <?php echo json_encode(intval($this->Params->get("ozio_nano_thumbnailWidth", "230"))); ?>,
+		maxItemsPerLine: <?php echo json_encode(intval($this->Params->get("ozio_nano_maxItemsPerLine", "0"))); ?>,
+		maxWidth: <?php echo json_encode(intval($this->Params->get("ozio_nano_maxWidth", "0"))); ?>,
+		touchAnimation: <?php echo json_encode(intval($this->Params->get("ozio_nano_touchAnimation", "1"))); ?>,
+		galleryToolbarWidthAligned: <?php echo json_encode(intval($this->Params->get("ozio_nano_galleryToolbarWidthAligned", "1"))); ?>,
+		slideshowDelay: <?php echo json_encode(intval($this->Params->get("ozio_nano_slideshowDelay", "3000"))); ?>,
+		paginationMaxItemsPerPage: <?php echo json_encode(intval($this->Params->get("ozio_nano_paginationMaxItemsPerPage", "0"))); ?>,
+		paginationMaxLinesPerPage: <?php echo json_encode(intval($this->Params->get("ozio_nano_paginationMaxLinesPerPage", "0"))); ?>,
+		thumbnailDisplayInterval: <?php echo json_encode(intval($this->Params->get("ozio_nano_thumbnailDisplayInterval", "30"))); ?>,
+		thumbnailDisplayTransition: <?php echo json_encode(intval($this->Params->get("ozio_nano_thumbnailDisplayTransition", "1"))); ?>,
+		thumbnailLazyLoad: <?php echo json_encode(intval($this->Params->get("ozio_nano_thumbnailLazyLoad", "0"))); ?>,
+		thumbnailLazyLoadTreshold: <?php echo json_encode(intval($this->Params->get("ozio_nano_thumbnailLazyLoadTreshold", "100"))); ?>,
+
+		viewer: <?php echo json_encode($this->Params->get("ozio_nano_viewer", "internal")); ?>,
+		thumbnailLabel: <?php echo json_encode(
+							array(
+									'position'=>$this->Params->get("ozio_nano_thumbnailLabel_position", "overImageOnBottom"),
+									'display'=>intval($this->Params->get("ozio_nano_thumbnailLabel_display", "1")),
+									'displayDescription'=>intval($this->Params->get("ozio_nano_thumbnailLabel_displayDescription", "1")),
+							)); 
+						?>,
+				
+		thumbnailHoverEffect: <?php echo $this->Params->get("ozio_nano_thumbnailHoverEffect", "none")=='none'?'null':json_encode($this->Params->get("ozio_nano_thumbnailHoverEffect", "none")); ?>,
+		theme: <?php echo json_encode($this->Params->get("ozio_nano_theme", "default")); ?>,
+		colorScheme: <?php echo json_encode($this->Params->get("ozio_nano_colorScheme", "light")); ?>,
+		colorSchemeViewer: <?php echo json_encode($this->Params->get("ozio_nano_colorSchemeViewer", "light")); ?>,
+
+		kind: <?php echo json_encode($this->Params->get("ozio_nano_kind", "picasa")); ?>,
+		userID: <?php echo json_encode($this->Params->get("ozio_nano_userID", "110359559620842741677")); ?>,
+		displayBreadcrumb: <?php echo json_encode(intval($this->Params->get("ozio_nano_displayBreadcrumb", "1"))); ?>,
+		blackList: <?php echo json_encode($this->Params->get("ozio_nano_blackList", "")); ?>,
+		whiteList: <?php echo json_encode($this->Params->get("ozio_nano_whiteList", "")); ?>,
+		<?php
+		$albumList=$this->Params->get("ozio_nano_albumList", array());
+		if (!empty($albumList) && is_array($albumList) ){
+			if (count($albumList)==1){
+				list($albumid,$title)=explode($non_printable_separator,$albumList[0]);
+				$kind=$this->Params->get("ozio_nano_kind", "picasa");
+				if ($kind=='picasa'){
+					echo 'album:'.json_encode($albumid).",\n";
+				}else{
+					echo 'photoset:'.json_encode($albumid).",\n";
+				}
+			}else{
+				$albumTitles=array();
+				foreach ($albumList as $a){
+					list($albumid,$title)=explode($non_printable_separator,$a);
+					$albumTitles[]=$title;
+				}
+				echo 'albumList:'.json_encode(implode('|',$albumTitles)).",\n";
+			}
+		}		
+		?>
+				
+
+	});
+
+});
